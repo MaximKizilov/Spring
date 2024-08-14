b1415f83b15dd9081e62b78d3d84581ebd7f1f53
“Maxim <kizilov2016@yandex.ru>
2024-08-14 23:31:38 +1000

    one commit

--- src/main/java/ru/netology/spring/config/JavaConfig.java
+++ src/main/java/ru/netology/spring/config/JavaConfig.java
@@ -10,13 +10,13 @@ import ru.netology.spring.systemProfile.SystemProfile;
 @Configuration
 public class JavaConfig {
     @Bean
-    @ConditionalOnProperty(value = "ru.netology.profile.dev", name = "dev", havingValue = "true")
+    @ConditionalOnProperty(value = "ru.netology.profile.dev",  havingValue = "true")
     public SystemProfile devProfile() {
         return new DevProfile();
     }
 
     @Bean
-    @ConditionalOnProperty(value = "ru.netology.profile", name = "dev", havingValue = "false")
+    @ConditionalOnProperty(value = "ru.netology.profile.dev",  havingValue = "false")
     public SystemProfile prodProfile() {
         return new ProductionProfile();
     }
